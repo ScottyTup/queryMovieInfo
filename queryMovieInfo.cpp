b32f558a7f98d279f8a6c1b0fadbf75cdc614aaf
Scott Tupper <Scott Tupper>
2016-02-20 01:01:53 -0500

    Add a function to run terminal commands. Test function by searching for Kurt Russell on Google.

--- queryMovieInfo.cpp
+++ queryMovieInfo.cpp
@@ -1,16 +1,26 @@
 // queryMovieInfo.cpp : Defines the entry point for the console application.
 //
 
-// System includes
-#include "stdafx.h"
-#include <iostream>
-
 #include "queryMovieInfo.hpp"
 
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	// Do a Google search for 'Kurt Russell' and output what comes back
+	std::string output = exec("curl -e http://www.my-aja-site.com \"http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=Kurt%20Russell\"");
+	std::cout << output << std::endl;
     return 0;
 }
 
+// Runs a terminal command and returns the output
+std::string exec(const char* cmd) {
+    std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) return "ERROR";
+    char buffer[128];
+    std::string result = "";
+    while (!feof(pipe.get())) {
+        if (fgets(buffer, 128, pipe.get()) != NULL)
+            result += buffer;
+    }
+    return result;
+}
--- queryMovieInfo.hpp
+++ queryMovieInfo.hpp
@@ -1,2 +1,11 @@
 // queryMovieInfo.hpp : Defines the entry point for the console application.
 //
+
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <memory>
+
+#include "stdafx.h"
+
+std::string exec(const char* cmd);
